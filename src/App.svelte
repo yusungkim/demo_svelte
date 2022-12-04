0da0677a253682b6484677f584d767b82666fe87
YusungKim <yusungkim@me.com>
2022-12-04 23:36:09 +0900

    contact card

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,69 @@
 <script>
+  import ContactCard from "./lib/ContactCard.svelte";
   import Counter from "./lib/Counter.svelte";
+
+  let name = "Kim";
+  let age = 39;
+  let jobTitle = "Scientist";
+  let description = "Has Great Skills";
+  let userImage = "https://via.placeholder.com/300/09f/fff.png";
+
+  $: if (name == "Kim") {
+    age = 39;
+  }
+  $: uppercaseName = name.toUpperCase();
+  $: console.log(name, age);
+
+  const increaseAge = () => age++;
+  const updateName = (e) => (name = e.target.value);
 </script>
 
 <main class="max-w-7xl mx-auto px-8 sm:px-16">
-  <section class="pt-6">
-  <div class="flex-col">
-    <h2 class="text-4xl font-semibold pb-5">Counter demo</h2>
-    <Counter />
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 h-20">
-      {#each [1,2,3,4] as _}
-        <Counter />
-     {/each}
+  <section class="p-6 ">
+    <div class="flex-col">
+      <h2 class="text-4xl font-semibold pb-5">Counter demo</h2>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 h-20"
+      >
+        {#each [1, 2, 3, 4] as _}
+          <Counter />
+        {/each}
+      </div>
     </div>
-      
-  </div>
-</section>
+  </section>
+  <section class="p-6">
+    <h2 class="text-4xl font-semibold my-2">
+      Hello {uppercaseName}, My age is {age}!
+    </h2>
+    <button
+      on:click={increaseAge}
+      class="hover:scale-105 active:bg-red-700 p-2 bg-red-600 text-white rounded-md"
+      >Increase Age</button
+    >
+    <div class="flex justify-between py-2">
+      <input
+        class="border-2 rounded-md p-1"
+        type="text"
+        value={name}
+        placeholder="name"
+        on:input={updateName}
+      />
+      <input
+        class="border-2 rounded-md p-1"
+        type="text"
+        placeholder="job"
+        bind:value={jobTitle}
+      />
+      <input
+        class="border-2 rounded-md p-1"
+        type="text"
+        placeholder="job"
+        bind:value={userImage}
+      />
+    </div>
+    <textarea bind:value={description} class="border-2 w-full max-h-max" />
+  </section>
+  <section class="p-6">
+    <ContactCard userName={name} {jobTitle} {description} {userImage} />
+  </section>
 </main>
---
+++ src/lib/ContactCard.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let userName = "User Name";
+  export let jobTitle = "Job Title";
+  export let description = "A short description here.";
+  export let userImage;
+</script>
+
+<div class="border-2 shadow-md rounded-md">
+  <div class="h-28 flex gap-2 items-center p-2 ">
+    {#if (userImage)}
+      <img class="w-24 aspect-square bg-stone-400 rounded-md" src={userImage} alt={userName} />
+    {:else}
+      <div class="w-24 aspect-square bg-stone-400 rounded-md" />  
+    {/if}
+    <div class="flex flex-col h-full justify-around">
+      <h1 class="text-xl font-bold font-serif">{userName}</h1>
+      <h2 class="text-lg">{jobTitle}</h2>
+    </div>
+  </div>
+  <hr class="border-1" />
+  <div class="h-32 max-h-min p-2 flex items-center">
+    <p>{description}</p>
+  </div>
+</div>
